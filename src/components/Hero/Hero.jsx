4a1af99b3003f35b090d7ef4f6503cf8ad6819b2
charishma <charishmaalam8@gmail.com>
2025-07-04 01:42:10 +0530

    updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Charishma</h1>
         <p className={styles.description}>
         Motivated and detail-oriented web developer with a strong foundation in data structures and algorithms. Proficient in building responsive, user-friendly web applications using modern technologies, with a proven track record of solving complex algorithmic problems. Experienced in full-stack development and implementing secure authentication systems. Currently expanding skill set in Machine Learning with hands-on experience in frameworks such as Scikit-learn, TensorFlow, and Keras.
         </p>
